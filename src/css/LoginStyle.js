e19c6fe9b69918e598ca1ea86916980ab3a75960
Umair Shoukat <umairshoukatryk19@gmail.com>
2023-11-02 21:30:52 +0500

    rechange LoginStyle to its previous version

--- src/css/LoginStyle.js
+++ src/css/LoginStyle.js
@@ -16,7 +16,6 @@ const LoginStyle = StyleSheet.create({
     txt: { color: 'black', fontWeight: 'bold' },
     main:{height:'auto'}
 });
-/////////////dydhjdjshfhsfhdhdfjdnfhfnfhfnfbghbhd///////dhbhnjndnf
-/////ffjdfdjfdjfdhfdfjdnfdnfjdfsjfnfffgfsv v  h dgjngjdnhbfsmdndjnddjndmkmkmmnmkdmfkdmkfm
+
 
 export default LoginStyle;
